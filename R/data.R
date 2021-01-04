5c4f3356a96bf03c385abc37ff3b212fc88314b4
hrodmn <henry.rodman@gmail.com>
2021-01-04 17:36:16 -0600

    add species crosswalk data object
    #6

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' species
+#'
+#' A dataframe relating the 'Species Group' in RP NC-250 to records in FIA's
+#' ref_species table
+#'
+#' @format dataframe:
+#' \describe{
+#'   \item{species_group}{species group label from paper}
+#'   \item{scientific_name}{Genus species}
+#'   \item{common_name}{common name}
+#'   \item{spcd}{numeric FIA species code}
+#' }
+#' @source data-raw/species.R
+"species"
---
+++ data-raw/species.R
@@ -0,0 +1,123 @@
+library(tidyverse)
+
+# species groups are present in all of the tables
+table1 <- vroom::vroom("inst/csv/table1.csv")
+
+# get ref_species table from FIA
+ref_species <- tidyFIA::ref_tables[["species"]] %>%
+  transmute(
+    common_name,
+    spcd,
+    scientific_name = paste(genus, species)
+  )
+
+# use Appendix III from original paper to generate crosswalk between groups and
+# actual species
+species_list <- list(
+  "Jack pine" = "Pinus banksiana",
+  "Red pine" = "Pinus resinosa",
+  "White pine" = "Pinus strobus",
+  "White spruce" = "Picea glauca",
+  "Black spruce" = "Picea mariana",
+  "Balsam fir" = "Abies balsamea",
+  "Hemlock" = "Tsuga canadensis",
+  "Tamarack" = "Larix laricina",
+  "Northern white-cedar" = "Thuja occidentalis",
+  "Other softwoods" = c(
+    "Pinus sylvestris",
+    "Pinus nigra",
+    "Juniperus virginiana",
+    "Larix decidua",
+    "Picea abies",
+    "Pseudotsuga menziesii"
+  ),
+  "White oak" = c(
+    "Quercus alba",
+    "Quercus bicolor",
+    "Quercus macrocarpa",
+    "Quercus muehlenbergii"
+  ),
+  "Red oak" = c(
+    "Quercus rubra",
+    "Quercus velutina",
+    "Quercus palustris",
+    "Quercus ellipsoidalis"
+  ),
+  "Hickory" = c(
+    "Carya cordiformis",
+    "Carya ovata",
+    "Carya glabra"
+  ),
+  "Basswood" = "Tilia americana",
+  "Beech" = "Fagus grandifolia",
+  "Yellow birch" = "Betula alleghaniensis",
+  "Hard maple" = c(
+    "Acer nigrum",
+    "Acer saccharum"
+  ),
+  "Soft maple" = c(
+    "Acer rubrum",
+    "Acer saccharinum" # silver maple but species is misspecified in paper
+  ),
+  "Elm" = c(
+    "Ulmus americana",
+    "Ulmus rubra",
+    "Ulmus thomasii"
+  ),
+  "Black ash" = "Fraxinus nigra",
+  "White & green ash" = c(
+    "Fraxinus americana",
+    "Fraxinus pennsylvanica"
+  ),
+  "Sycamore" = "Platanus occidentalis",
+  "Cottonwood" = "Populus deltoides",
+  "Willow" = "Salix spp.",
+  "Hackberry" = "Celtis occidentalis",
+  "Balsam poplar" = "Populus balsamifera",
+  "Bigtooth aspen" = "Populus grandidentata",
+  "Quaking aspen" = "Populus tremuloides",
+  "Paper birch" = "Betula papyrifera",
+  "River birch" = "Betula nigra",
+  "Sweetgum" = "Liquidambar styraciflua",
+  "Tupelo" = "Nyssa sylvatica",
+  "Black cherry" = "Prunus serotina",
+  "Black walnut" = "Juglans nigra",
+  "Butternut" = "Juglans cinerea",
+  "Yellow poplar" = "Liriodendron tulipifera",
+  "Other hardwoods" = c(
+    "Acer negundo"
+  ),
+  "Noncommercial species" = c(
+    "Acer pensylvanicum",
+    "Acer spicatum",
+    "Ailanthus altissima", # Adanthus altessimor in paper
+    "Carpinus caroliniana",
+    "Ostrya virginiana",
+    "Cercis canadensis",
+    "Crataegus spp.",
+    "Prunus pensylvanica",
+    "Prunus virginiana",
+    "Sorbus americana"
+  )
+)
+
+species_groups <- table1 %>%
+  select(
+    species_group
+  )
+
+
+species <-
+  purrr::imap_dfr(
+    .x = species_list,
+    .f = ~ tibble::tibble(
+      species_group = .y,
+      scientific_name = .x
+    )
+  ) %>%
+  left_join(
+    ref_species,
+    by = "scientific_name"
+  )
+
+usethis::use_data(species, overwrite = TRUE)
