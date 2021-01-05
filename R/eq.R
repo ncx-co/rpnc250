23b323e7b6aa10bf046d50be5ada2688b0b9aeaf
hrodmn <henry.rodman@gmail.com>
2021-01-05 08:41:07 -0600

    add function to compute volume
    #3, #4

--- R/eq.R
+++ R/eq.R
@@ -93,3 +93,87 @@ apply_height_eq <- function(dbh, site_index, top_dob, stand_basal_area,
   4.5 + b1 * (1 - exp(-b2 * dbh))^b3 * site_index^b4 * t_term^b5 *
     stand_basal_area^b6
 }
+
+#' Estimate volume
+#'
+#' Estimate volume to specified height along stem in either cubic feet or
+#' International 1/4 board feet. The equation is applied by the function
+#' \code{\link{apply_volume_equation}}
+#'
+#' @param vol_type units for volume estimates, either \code{cuft} or
+#' \code {bdft}
+#' @param height vector of heights to which to estimate volume
+#' @inheritParams estimate_height
+#'
+#' @return vector of volume estimates
+#' @examples
+#' estimate_volume(
+#'   spcd = c(541, 371, 95, 73),
+#'   dbh = c(10, 11, 12, 13),
+#'   height = 40, 40, 24, 24,
+#'   vol_type = "cuft"
+#' )
+#' @export
+
+estimate_volume <- function(spcd, dbh, height, vol_type = c("cuft", "bdft")) {
+  # verify volume type as either cuft or bdft
+  vol_type <- match.arg(vol_type, choices = c("cuft", "bdft"))
+
+  # assign species_group
+  species_groups <- assign_species_group(spcd)
+
+  # choose coef table 2 or 3 depending on volume type
+  coef_table <- switch(
+    vol_type,
+    cuft = table2,
+    bdft = table3
+  )
+
+  # get coefficients
+  match_idx <- match(species_groups, coef_table$species_group)
+  coeffs <- coef_table[match_idx, ]
+
+  # estimate height
+  apply_volume_eq(
+    dbh = dbh,
+    height = height,
+    b0 = coeffs$b0,
+    b1 = coeffs$b1
+  )
+}
+
+#' Apply volume equation
+#'
+#' Apply the volume equation (eq. 1 in publication) from RP NC-250 using
+#' provided dbh, height to merchantable limit, and coefficients from Table 1 in
+#' the publication.
+#'
+#' The volume equation is as follows:
+#' \deqn{V = b_0 + b_1 D^2 H}
+#'
+#' Where:
+#' \itemize{
+#'   \item \eqn{V} is the volume in either cubic feet or board feet
+#'   \item \eqn{b_0} and \eqn{b_1} are species specific coefficients from Table
+#'   1
+#'   \item \eqn{D} is diameter at breast height in inches
+#'   \item \eqn{H} height to which volume is to be computed
+#' }
+#'
+#' @param b0,b1 vector of model coefficients from Table 2 or Table 3
+#' @inheritParams estimate_volume
+#'
+#' @return vector of volume estimates
+#' @examples
+#' apply_volume_eq(
+#'   dbh = 10,
+#'   height = 40
+#'   b0 = table2$b0[1],
+#'   b1 = table2$b1[1]
+#' )
+
+apply_volume_eq <- function(dbh, height, b0, b1) {
+
+  b0 + b1 * dbh^2 * height
+
+}
